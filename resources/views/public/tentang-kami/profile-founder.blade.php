6df4dae300cfdb7aaca2f9b706ccf130023dd1c2
AdityaSundawa <adityasundawa.co@gmail.com>
2023-09-22 11:24:09 +0700

    Revisi Struktur Organisasi

--- resources/views/public/tentang-kami/profile-founder.blade.php
+++ resources/views/public/tentang-kami/profile-founder.blade.php
@@ -24,7 +24,7 @@
                         <h4>Mr.Dwi Larso</h4>
                         <span>Dewan Penasehat</span>
                         <div class="team-social mt-15">
-                            <p><strong>Minat Bakat:</strong> <br>Matematika,Bahasa Inggris,Ilmu Pengetahuan Alam</p>
+                            <p>Direktur Beasiswa LPDP                            </p>
                             <a href="#"><i class="fab fa-facebook-f"></i></a>
                             <a href="#"><i class="fab fa-twitter"></i></a>
                             <a href="#"><i class="fab fa-instagram"></i></a>
@@ -44,7 +44,7 @@
                         <h4>Mrs.Firda D</h4>
                         <span>Dewan Penasehat</span>
                         <div class="team-social mt-15">
-                            <p><strong>Minat Bakat:</strong> <br>Matematika,Bahasa Inggris,Ilmu Pengetahuan Alam</p>
+                            <p>Principal  of Sampoerna Academy Sentul                            </p>
                             <a href="#"><i class="fab fa-facebook-f"></i></a>
                             <a href="#"><i class="fab fa-twitter"></i></a>
                             <a href="#"><i class="fab fa-instagram"></i></a>
@@ -63,7 +63,7 @@
                         <h4>Mr.D. Marhen</h4>
                         <span>Dewan Penasehat</span>
                         <div class="team-social mt-15">
-                            <p><strong>Minat Bakat:</strong> <br>Matematika,Bahasa Inggris,Ilmu Pengetahuan Alam</p>
+                            <p>Direktur Pemasaran Pariwisata Kemenparekraf</p>
                             <a href="#"><i class="fab fa-facebook-f"></i></a>
                             <a href="#"><i class="fab fa-twitter"></i></a>
                             <a href="#"><i class="fab fa-instagram"></i></a>
@@ -82,7 +82,7 @@
                         <h4>Yulenda P</h4>
                         <span>Dewan Pembina</span>
                         <div class="team-social mt-15">
-                            <p><strong>Minat Bakat:</strong> <br>Matematika,Bahasa Inggris,Ilmu Pengetahuan Alam</p>
+                            <p>Wali Murid</p>
                             <a href="#"><i class="fab fa-facebook-f"></i></a>
                             <a href="#"><i class="fab fa-twitter"></i></a>
                             <a href="#"><i class="fab fa-instagram"></i></a>
@@ -101,7 +101,7 @@
                         <h4>A. Sofiandi</h4>
                         <span>Dewan Pembina</span>
                         <div class="team-social mt-15">
-                            <p><strong>Minat Bakat:</strong> <br>Matematika,Bahasa Inggris,Ilmu Pengetahuan Alam</p>
+                            <p>Wali Murid</p>
                             <a href="#"><i class="fab fa-facebook-f"></i></a>
                             <a href="#"><i class="fab fa-twitter"></i></a>
                             <a href="#"><i class="fab fa-instagram"></i></a>
@@ -120,7 +120,7 @@
                         <h4>Imelda S</h4>
                         <span>Dewan Pembina</span>
                         <div class="team-social mt-15">
-                            <p><strong>Minat Bakat:</strong> <br>Matematika,Bahasa Inggris,Ilmu Pengetahuan Alam</p>
+                            <p>Wali Murid</p>
                             <a href="#"><i class="fab fa-facebook-f"></i></a>
                             <a href="#"><i class="fab fa-twitter"></i></a>
                             <a href="#"><i class="fab fa-instagram"></i></a>
@@ -139,7 +139,7 @@
                         <h4>Frida S</h4>
                         <span>Dewan Pembina</span>
                         <div class="team-social mt-15">
-                            <p><strong>Minat Bakat:</strong> <br>Matematika,Bahasa Inggris,Ilmu Pengetahuan Alam</p>
+                            <p>Wali Murid</p>
                             <a href="#"><i class="fab fa-facebook-f"></i></a>
                             <a href="#"><i class="fab fa-twitter"></i></a>
                             <a href="#"><i class="fab fa-instagram"></i></a>
@@ -158,7 +158,7 @@
                         <h4>Mr. Jefri</h4>
                         <span>Dewan Penasehat</span>
                         <div class="team-social mt-15">
-                            <p><strong>Minat Bakat:</strong> <br>Matematika,Bahasa Inggris,Ilmu Pengetahuan Alam</p>
+                            <p>Wali Murid</p>
                             <a href="#"><i class="fab fa-facebook-f"></i></a>
                             <a href="#"><i class="fab fa-twitter"></i></a>
                             <a href="#"><i class="fab fa-instagram"></i></a>
@@ -177,7 +177,7 @@
                         <h4>Mrs.Pricillia</h4>
                         <span>Dewan Penasehat</span>
                         <div class="team-social mt-15">
-                            <p><strong>Minat Bakat:</strong> <br>Matematika,Bahasa Inggris,Ilmu Pengetahuan Alam</p>
+                            <p>Wali Murid</p>
                             <a href="#"><i class="fab fa-facebook-f"></i></a>
                             <a href="#"><i class="fab fa-twitter"></i></a>
                             <a href="#"><i class="fab fa-instagram"></i></a>
@@ -260,6 +260,106 @@
                         <span>Pengajar</span>
                         <div class="team-social mt-15">
                             <p><strong>Minat Bakat:</strong> <br>Matematika,Bahasa Inggris,Bahasa Indonesia</p>
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        <div class="col-lg-4 col-md-6">
+                <div class="single-team text-center mb-30">
+                    <div class="team-thumb">
+                        <div class="brd">
+                            <img src="{{url('/')}}/assets/img/direktur/vector.png" class="img" alt="Gambar tentang kami">
+                        </div>
+                    </div>
+                    <div class="team-info">
+                        <h4>xxxxx</h4>
+                        <span>Cief Operation Officer</span>
+                        <div class="team-social mt-15">
+                            <p><strong>Minat Bakat:</strong> <br>Matematika,Bahasa Inggris,Ilmu Pengetahuan Alam</p>
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        <div class="col-lg-4 col-md-6">
+                <div class="single-team text-center mb-30">
+                    <div class="team-thumb">
+                        <div class="brd">
+                            <img src="{{url('/')}}/assets/img/direktur/vector.png" class="img" alt="Gambar tentang kami">
+                        </div>
+                    </div>
+                    <div class="team-info">
+                        <h4>xxxxx</h4>
+                        <span>Cief People</span>
+                        <div class="team-social mt-15">
+                            <p><strong>Minat Bakat:</strong> <br>Matematika,Bahasa Inggris,Ilmu Pengetahuan Alam</p>
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        <div class="col-lg-4 col-md-6">
+                <div class="single-team text-center mb-30">
+                    <div class="team-thumb">
+                        <div class="brd">
+                            <img src="{{url('/')}}/assets/img/direktur/vector.png" class="img" alt="Gambar tentang kami">
+                        </div>
+                    </div>
+                    <div class="team-info">
+                        <h4>xxxxx</h4>
+                        <span>Cief Administrasi</span>
+                        <div class="team-social mt-15">
+                            <p><strong>Minat Bakat:</strong> <br>Matematika,Bahasa Inggris,Ilmu Pengetahuan Alam</p>
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        <div class="col-lg-4 col-md-6">
+                <div class="single-team text-center mb-30">
+                    <div class="team-thumb">
+                        <div class="brd">
+                            <img src="{{url('/')}}/assets/img/direktur/vector.png" class="img" alt="Gambar tentang kami">
+                        </div>
+                    </div>
+                    <div class="team-info">
+                        <h4>xxxxx</h4>
+                        <span>Chief Fund Rising</span>
+                        <div class="team-social mt-15">
+                            <p><strong>Minat Bakat:</strong> <br>Matematika,Bahasa Inggris,Ilmu Pengetahuan Alam</p>
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        <div class="col-lg-4 col-md-6">
+                <div class="single-team text-center mb-30">
+                    <div class="team-thumb">
+                        <div class="brd">
+                            <img src="{{url('/')}}/assets/img/direktur/vector.png" class="img" alt="Gambar tentang kami">
+                        </div>
+                    </div>
+                    <div class="team-info">
+                        <h4>xxxxx</h4>
+                        <span>Chief Digital Marketing</span>
+                        <div class="team-social mt-15">
+                            <p><strong>Minat Bakat:</strong> <br>Matematika,Bahasa Inggris,Ilmu Pengetahuan Alam</p>
                             <a href="#"><i class="fab fa-facebook-f"></i></a>
                             <a href="#"><i class="fab fa-twitter"></i></a>
                             <a href="#"><i class="fab fa-instagram"></i></a>
